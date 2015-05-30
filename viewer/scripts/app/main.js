e70f2f24476ae4b6361708b1a5b25852ebf1dc66
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2015-05-29 18:06:31 -0600

    Disabled automatic update in viewer.

--- viewer/scripts/app/main.js
+++ viewer/scripts/app/main.js
@@ -1,7 +1,11 @@
-define(['./tca', './graph'], function (tca, graph) {
+define(['./tca', './graph', 'jquery'], function (tca, graph, $) {
   
   tca.start();
   
+  $('#step').on('click', function(){
+    tca.stop();
+    tca.update();
+  });
   
   document.body.appendChild(graph.view);
   graph.animate();
